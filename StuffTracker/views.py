414b80ab85ed1748f87bbdbf2890eb7b09107d32
Jayson Rock <rock@uwm.edu>
2021-03-30 07:56:53 -0500

    create user

--- StuffTracker/views.py
+++ StuffTracker/views.py
@@ -7,25 +7,35 @@ class Home(View):
     def get(self,request):
         return render(request,"home.html",{})
     def post(self,request):
+        noSuchUser = False
+        badPassword = False
         try:
             m = MyUser.objects.get(name=request.POST['name'])
-            isValid = (m.password == request.POST['password'])
+            badPassword = (m.password != request.POST['password'])
         except:
-            pass
-        if isValid:
+            noSuchUser = True
+        if noSuchUser:
+            m = MyUser(name=request.POST['name'], password = request.POST['password'])
+            m.save()
+            request.session["name"] = m.name
             return redirect("/things/")
+        elif badPassword:
+            return render(request,"home.html",{"message":"bad password"})
         else:
-            return render(request,"home.html",{})
+            request.session["name"] = m.name
+            return redirect("/things/")
 
 
 class Things(View):
     def get(self,request):
-        things = map(str,list(Stuff.objects.all()))
-        return render(request, "things.html", {"things":things})
+        m = request.session["name"]
+        things = map(str,list(Stuff.objects.filter(owner__name=m)))
+        return render(request, "things.html", {"name":m, "things":things})
     def post(self,request):
+        m = request.session["name"]
         s = request.POST.get('stuff','')
         if s != '':
-            newThing = Stuff(name=s)
+            newThing = Stuff(name=s,owner=MyUser.objects.get(name=m))
             newThing.save()
-        things = map(str,list(Stuff.objects.all()))
-        return render(request, "things.html", {"things":things})
+        things = map(str,list(Stuff.objects.filter(owner__name=m)))
+        return render(request, "things.html", {"name":m, "things":things})
